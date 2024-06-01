83d2f0a2e1387eda944bf5da3891c839a34daea3
Anuragvishwakarma95 <133267316+Anuragvishwakarma95@users.noreply.github.com>
2024-06-01 14:55:47 +0530

    Add files via upload

---
+++ homepage/home.js
@@ -0,0 +1,40 @@
+let menu = document.querySelector("#menuBars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () =>{
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+
+};
+
+
+window.onscroll = () =>{
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+
+
+};
+
+
+var swiper = new Swiper(".ReviewSlide", {
+    slidesperView: 1,
+    grabCuror: true,
+    loop: true,
+    spaceBetween: 10,
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1050: {
+            slidesPerView: 3,
+        },
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+});
